fdb3a8dc6c607c3c7621369de1cace6bbc76ea87
SUPERPED1 <136564583+SUPERPED1@users.noreply.github.com>
2026-03-24 19:31:30 +0700

    Implement table drop checks in DDL.sql
    Added checks to drop existing tables before creating new ones in DDL script.

--- script/bronze/DDL.sql
+++ script/bronze/DDL.sql
@@ -1,4 +1,13 @@
--- DDL
+-- ===================================================
+-- CREATE TABLE base on data in CSV file
+-- ===================================================
+-- !!!!!!!!!!!!!!!!!!!!WARNING!!!!!!!!!!!!!!!!!!!!!!!!
+-- Running this command can cause deleted data in the
+-- table carefully checking before running this command
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+-- Checking is table EX. bronze.crm_cust_info is occur than Drop table before create the new one
+-- Every column and datatype base on column and data in CSV file
 IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
@@ -10,8 +19,9 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_gndr NVARCHAR(50),
 	cst_create_date DATE
 );
-GO
 
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info (
 	prd_id INT,
 	prd_key NVARCHAR(50),
@@ -21,9 +31,10 @@ CREATE TABLE bronze.crm_prd_info (
 	prd_start_dt DATE,
 	prd_end_dt DATE
 );
-GO
 
-CREATE TABLE bronze.crm_salse_details (
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num INT,
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
@@ -34,21 +45,24 @@ CREATE TABLE bronze.crm_salse_details (
 	sls_quantity INT,
 	sls_price INT
 );
-GO
 
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12 (
 	cid	NVARCHAR(50),
 	bdate DATE,
 	gen NVARCHAR(50)
 );
-GO
 
-CREATE TABLE bronze.erp_log_a101 (
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101 (
 	cid	NVARCHAR(50),
 	cntry NVARCHAR(50)
 );
-GO
 
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
 CREATE TABLE bronze.erp_px_cat_g1v2 (
 	id NVARCHAR(50),
 	cat NVARCHAR(50),
